04d5012ce92faabbf6dadc0a85dd74ebe0705073
Ajax <ajax@ajax.lan>
2020-07-02 05:58:42 +0800

    0.1update-m

--- Monthsary/script.js
+++ Monthsary/script.js
@@ -1,13 +1,19 @@
 
 function clickMe(){
-    document.getElementById("demo1").innerHTML = "Hi Love! hahaha gulat ka ba? I hope you liked this simple presentation I haved made po. With all my best and effort charoot!! hehehe Well, without further ado. I just wanna say";
-    document.getElementById("demo2").innerHTML = "HAPPY 5th MONTHSARY LOVE &#128152 &#128149";
-    document.getElementById("demo3").innerHTML = "I cannot measure the things that happened to us inspite of the pandemic, lockdown, getting fat and etc. Hahaha we are still here taking care of each other. I know you already know this but still 'there is nothing I can ask' I already have it all, You &#128150 &#128151 &#128152 &#128153 &#128154 &#128155 &#128158";
-    document.getElementById("demo4").innerHTML = "Hehe wala na akong ibang makuhang pic love eh sorry. HAHAHA pero ang cute naman eh. HIHIHIHIHIHHHHIHHIHIHIHIHi&#128150 &#128151 &#128152 &#128153 &#128154 &#128155 &#128158";
-    document.getElementById("junk1").innerHTML = "";
+    document.getElementById("demo1").innerHTML = "Hi Love! hahaha gulat ka ba? I hope you like this simple presentation I have made po. With all my best and effort charoot!! hehehe Well, without further ado. I just wanna say";
+    document.getElementById("demo2").innerHTML = "HAPPY 5th MONTHSARY LOVE &#128152 &#128149 &#128152";
+    document.getElementById("demo3").innerHTML = "I cannot measure the things that happaned to us inspite of the pandemic, lockdown, getting fat and etc. Hahaha we are still here taking care of each other. I know you already know this but still 'there is nothing I can ask for' I already have said it but I already have it all, which is You &#128150 &#128151 &#128152 &#128153 &#128154 &#128155 &#128158";
+    document.getElementById("demo4").innerHTML = "Hehe wala na akong ibang makuhang pic love eh sorry. HAHAHA. HIHIHIHIHIHHHHIHHIHIHIHIHi&#128150 &#128151 &#128152 &#128153 &#128154 &#128155 &#128158";
+    document.getElementById("junk1").innerHTML = "I LOVE YOU SO MUCH JUSTINE MELITON REYES &#128150 &#128151 &#128152 &#128153 &#128154 &#128155 &#128158";
     document.getElementById("junk2").innerHTML = "";
     document.getElementById("junk3").innerHTML = "";
     document.getElementById("junk4").innerHTML = "";
+    var z = document.createElement("IMG");
+    z.setAttribute("src", "https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-9/106425710_1140606819672249_752568048779905597_n.jpg?_nc_cat=101&_nc_sid=b96e70&_nc_ohc=qD03yj_CpEIAX8B_8tH&_nc_ht=scontent.fceb2-1.fna&oh=58adb3ae5a1d540428292309670e871a&oe=5F210285");
+    z.setAttribute("width", "180");
+    z.setAttribute("height", "300");
+    z.setAttribute("alt", "TATABS");
+    document.body.appendChild(z);
     var x = document.createElement("IMG");
     x.setAttribute("src", "https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-0/p280x280/80197622_727737351376790_4370231148018586031_n.jpg?_nc_cat=106&_nc_sid=b96e70&_nc_ohc=abDncyEowGYAX-ke8tw&_nc_ht=scontent.fceb2-1.fna&_nc_tp=6&oh=7b24e294d0b6473ccb08a0dd46600eb0&oe=5F21261D");
     x.setAttribute("width", "180");
@@ -16,6 +22,7 @@ function clickMe(){
     document.body.appendChild(x);
     var y = document.createElement("IMG");
     y.setAttribute("src", "https://scontent.fceb2-2.fna.fbcdn.net/v/t1.15752-0/p280x280/106668864_3432132103677793_8052434156668030834_n.jpg?_nc_cat=104&_nc_sid=b96e70&_nc_ohc=NI8oYY0mvgUAX_bHUQo&_nc_ht=scontent.fceb2-2.fna&_nc_tp=6&oh=2964a23122da45e5384673e84ef33ec0&oe=5F22ABCF");
+    y.setAttribute("id", "img3");
     y.setAttribute("width", "180");
     y.setAttribute("height", "220");
     y.setAttribute("alt", "BAMBAM");
