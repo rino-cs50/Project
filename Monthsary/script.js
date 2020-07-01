0e6b2d34d998f0364c56b5b762409ccf8d0a5dd6
Ajax <ajax@ajax.lan>
2020-07-02 02:36:09 +0800

    Upload-Monthsary-Folder

---
+++ Monthsary/script.js
@@ -0,0 +1,25 @@
+
+function clickMe(){
+    document.getElementById("demo1").innerHTML = "Hi Love! hahaha gulat ka ba? I hope you liked this simple presentation I haved made po. With all my best and effort charoot!! hehehe Well, without further ado. I just wanna say";
+    document.getElementById("demo2").innerHTML = "HAPPY 5th MONTHSARY LOVE &#128152 &#128149";
+    document.getElementById("demo3").innerHTML = "I cannot measure the things that happened to us inspite of the pandemic, lockdown, getting fat and etc. Hahaha we are still here taking care of each other. I know you already know this but still 'there is nothing I can ask' I already have it all, You &#128150 &#128151 &#128152 &#128153 &#128154 &#128155 &#128158";
+    document.getElementById("demo4").innerHTML = "Hehe wala na akong ibang makuhang pic love eh sorry. HAHAHA pero ang cute naman eh. HIHIHIHIHIHHHHIHHIHIHIHIHi&#128150 &#128151 &#128152 &#128153 &#128154 &#128155 &#128158";
+    document.getElementById("junk1").innerHTML = "";
+    document.getElementById("junk2").innerHTML = "";
+    document.getElementById("junk3").innerHTML = "";
+    document.getElementById("junk4").innerHTML = "";
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-0/p280x280/80197622_727737351376790_4370231148018586031_n.jpg?_nc_cat=106&_nc_sid=b96e70&_nc_ohc=abDncyEowGYAX-ke8tw&_nc_ht=scontent.fceb2-1.fna&_nc_tp=6&oh=7b24e294d0b6473ccb08a0dd46600eb0&oe=5F21261D");
+    x.setAttribute("width", "180");
+    x.setAttribute("height", "220");
+    x.setAttribute("alt", "TATABS");
+    document.body.appendChild(x);
+    var y = document.createElement("IMG");
+    y.setAttribute("src", "https://scontent.fceb2-2.fna.fbcdn.net/v/t1.15752-0/p280x280/106668864_3432132103677793_8052434156668030834_n.jpg?_nc_cat=104&_nc_sid=b96e70&_nc_ohc=NI8oYY0mvgUAX_bHUQo&_nc_ht=scontent.fceb2-2.fna&_nc_tp=6&oh=2964a23122da45e5384673e84ef33ec0&oe=5F22ABCF");
+    y.setAttribute("width", "180");
+    y.setAttribute("height", "220");
+    y.setAttribute("alt", "BAMBAM");
+    document.body.appendChild(y);
+}
+
+
